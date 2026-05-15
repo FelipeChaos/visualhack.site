2b090eeb664ca81686454d210fb940daf296f13a
Felipe Suarez <126610069+FelipeChaos@users.noreply.github.com>
2026-05-14 21:33:08 -0500

    Submiting forms
    Submiting forms

--- js/app.js
+++ js/app.js
@@ -19,52 +19,36 @@ window.addEventListener('scroll', revealElements);
 revealElements();
 
 
-// FORMULARIO
+// FORMULARIO EMAILJS
 
 const form = document.getElementById('contact-form');
 
-form.addEventListener('submit', async function (e) {
+form.addEventListener('submit', function (e) {
 
     e.preventDefault();
 
-    const formData = new FormData(form);
-
     const message = document.getElementById('form-message');
 
-    try {
+    emailjs.sendForm(
+        'service_8cj0thm',
+        'template_2ldtole',
+        this
+    )
 
-        const response = await fetch(
-            'https://formspree.io/f/mwvydnpo',
-            {
-                method: 'POST',
-                body: formData,
-                headers: {
-                    'Accept': 'application/json'
-                }
-            }
-        );
-
-        if (response.ok) {
+        .then(function () {
 
             message.innerHTML = "Solicitud enviada correctamente.";
             message.style.color = "#25d366";
 
             form.reset();
 
-        } else {
+        }, function (error) {
 
-            message.innerHTML = "Error enviando formulario.";
+            console.error(error);
+
+            message.innerHTML = "Error enviando solicitud.";
             message.style.color = "red";
 
-        }
-
-    } catch (error) {
-
-        console.error(error);
-
-        message.innerHTML = "Error de conexión.";
-        message.style.color = "red";
-
-    }
+        });
 
 });
